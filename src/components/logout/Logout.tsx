2f1fa1f6030ab1df58d8513038cf9d9a793b29a5
Muhammad Muneeb <nadeemmk1900@gmail.com>
2025-02-11 18:16:21 +0500

    fix: change location of middlware from app to root

--- src/components/logout/Logout.tsx
+++ src/components/logout/Logout.tsx
@@ -6,12 +6,12 @@ import { signOut } from "next-auth/react";
 export default function Logout() {
   return (
     <>
-      <div
+      <button
         onClick={() => signOut()}
         className="bg-red-500 text-white px-3 py-2 cursor-pointer rounded"
       >
         Logout
-      </div>
+      </button>
     </>
   );
 }
